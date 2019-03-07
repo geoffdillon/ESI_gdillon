4ec56eeef2d89f24b8a3144c0d51c0e7fdebe8cd
Geoff Dillon <geoff_dillon@dell.com>
2019-03-07 16:52:38 -0600

    fix path typo
    $ should have been %

--- SLSBuild/build_im.bat
+++ SLSBuild/build_im.bat
@@ -3,7 +3,7 @@ rem IM workspace builds
 if "%1"=="" goto usage
 
 set BASE=%1
-set IM_PATH=$BASE%\dcsg5-im\software\embedded\nxp_lpc_18xx\source
+set IM_PATH=%BASE%\dcsg5-im\software\embedded\nxp_lpc_18xx\source
 
 echo Building IM
 call build_mcu.bat %IM_PATH%\bootloader bootloader Release
