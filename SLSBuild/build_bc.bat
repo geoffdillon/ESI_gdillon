72337fdf39a6b1b0b97267d7fe5f36f3dc69e8d8
Geoff Dillon <geoff_dillon@dell.com>
2019-03-07 17:02:27 -0600

    pretty up script
    add more comments and set echo off

--- SLSBuild/build_bc.bat
+++ SLSBuild/build_bc.bat
@@ -1,3 +1,4 @@
+@echo off
 rem build_bc.bat
 rem BC bootloader workspace
 if "%1"=="" goto usage
@@ -5,9 +6,13 @@ if "%1"=="" goto usage
 set BASE=%1
 set BC_PATH=$BASE%\dcsg5-bc\software\embedded\nxp_lpc_18xx\source
 
-echo Building BC
+echo *************Building bootloader
 call build_mcu.bat %BC_PATH%\bootloader bootloader Release
+
+echo *************Building FailSafeImage_BC
 call build_mcu.bat %BC_PATH%\fs_op_image FailSafeImage_BC Release
+
+echo *************Building OpImage_BC
 call build_mcu.bat %BC_PATH%\fs_op_image OpImage_BC Release
 
 goto:eof
--- SLSBuild/build_im.bat
+++ SLSBuild/build_im.bat
@@ -1,3 +1,4 @@
+@echo off
 rem build_im.bat
 rem IM workspace builds
 if "%1"=="" goto usage
@@ -5,9 +6,13 @@ if "%1"=="" goto usage
 set BASE=%1
 set IM_PATH=%BASE%\dcsg5-im\software\embedded\nxp_lpc_18xx\source
 
-echo Building IM
+echo *************Building bootloader
 call build_mcu.bat %IM_PATH%\bootloader bootloader Release
+
+echo *************Building FailSafeImage_IM
 call build_mcu.bat %IM_PATH%\fs_op_image FailSafeImage_IM Release
+
+echo *************Building OpImage_IM
 call build_mcu.bat %IM_PATH%\fs_op_image OpImage_IM Release
 
 goto:eof
