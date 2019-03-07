7e9efc30c413de0181ffbb2e2a7600f3c1097cda
Geoff Dillon <geoff_dillon@dell.com>
2019-03-07 16:29:56 -0600

    change BC to IM in IM script

--- SLSBuild/build_im.bat
+++ SLSBuild/build_im.bat
@@ -5,7 +5,7 @@ if "%1"=="" goto usage
 set BASE=%1
 set IM_PATH=$BASE%\dcsg5-im\software\embedded\nxp_lpc_18xx\source
 
-echo Building BC
+echo Building IM
 call build_mcu.bat %IM_PATH%\bootloader bootloader Release
 call build_mcu.bat %IM_PATH%\fs_op_image FailSafeImage_IM Release
 call build_mcu.bat %IM_PATH%\fs_op_image OpImage_IM Release
@@ -14,6 +14,6 @@ goto:eof
 
 :usage
 echo Usage:
-echo     build_bc.bat "basepath of BC repo"
+echo     build_im.bat "basepath of BC repo"
 echo Example:
-echo     build_bc.bat c:\work
+echo     build_im.bat c:\work
