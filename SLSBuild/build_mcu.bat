1d914faec87f574350c9948261625f6b14b57c03
Geoff Dillon <geoff_dillon@dell.com>
2019-03-08 09:57:39 -0600

    fix path building error
    sometime the path to MCUXpre3sso was not being set, %MCU% was being evaluated before execution in the SET MCUX= line, so had to set EnableDelayedExpansion to make it execute like a real script, duh.

--- SLSBuild/build_mcu.bat
+++ SLSBuild/build_mcu.bat
@@ -1,5 +1,6 @@
 @echo off
 rem Batch file to build MCUXpresso projects from command line
+setlocal EnableDelayedExpansion
 
 if "%1"=="" goto usage
 if "%2"=="" goto usage
@@ -13,9 +14,9 @@ set BUILDCONFIG=%3
 echo BUILDCONFIG is %BUILDCONFIG%
 SET NXP_HOME=c:\nxp
 if exist "%NXP_HOME%" (
-	for /F "tokens=* USEBACKQ" %%F in (`dir /AD /b %NXP_HOME%`) DO (set MCU=%%F) 
-	SET MCUX=%MCU%
-	echo Found MCUXpresso in "%NXP_HOME%\%MCUX%"
+	for /F "tokens=* USEBACKQ" %%F in (`dir /AD /b %NXP_HOME%`) DO (set MCU=%%F)
+	SET MCUX=!MCU!
+	echo Found MCUXpresso in "%NXP_HOME%\!MCUX!"
 ) else (
 	echo ERROR: MCUXpresso is not installed under %NXP_HOME%
 	goto:eof
@@ -27,8 +28,6 @@ set IDE=%NXP_HOME%\%MCUX%\ide\mcuxpressoidec.exe
 
 echo %PATH%|findstr /i /c:"%TOOLCHAIN_PATH:"=%">nul ||set PATH=%PATH%;%TOOLCHAIN_PATH%
 
-set LOGFILE=%2_%BUILDCONFIG%.log
-echo Begin build, logging to %LOGFILE%
 %IDE% -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "%1" -importAll "%1" -cleanBuild "%2/%BUILDCONFIG%"
 
 %IDE% -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "%1" -build "%2/%BUILDCONFIG%"
